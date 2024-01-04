18dfecc23fd9c1f090bfcad19e10be0e3a721c2b
Max <143605086+maxim513@users.noreply.github.com>
2024-01-04 05:20:26 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,33 @@
+
+    data.forEach(item => {
+    const data = generateRandomData();
+function main() {
+    const data = [];
+        console.log("Random Number: " + item);
+}
+}
+main();
+function generateRandomData() {
+    return data;
+    }
+    for (let i = 0; i < 10; i++) {
+
+    });
+
+        data.push(Math.floor(Math.random() * 100) + 1);    data.forEach(item => {
+    }
+function main() {
+    const data = generateRandomData();
+main();
+function generateRandomData() {
+}
+    });
+}
+    const data = [];
+
+        console.log("Random Number: " + item);
+
+
+    for (let i = 0; i < 10; i++) {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    return data;
